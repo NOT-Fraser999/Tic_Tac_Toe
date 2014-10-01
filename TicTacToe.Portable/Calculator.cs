ffed53d1e390b02d534c762861f72567838c33f7
Viv-Rajkumar <viv.rajkumar@maidsafe.net>
2014-10-01 11:49:02 +0100

    Style updates.

--- TicTacToe.Portable/Calculator.cs
+++ TicTacToe.Portable/Calculator.cs
@@ -1,11 +1,16 @@
 ﻿namespace TicTacToe.Portable {
   public class Calculator {
-    //Method to add two numbers
+    /// <summary>
+    /// Method to add two numbers
+    /// </summary>
+    /// <param name="numberOne">First Number</param>
+    /// <param name="numberTwo">Second Number</param>
+    /// <returns>Returns the result of addition</returns>
     public static int Add(int numberOne, int numberTwo) {
       return numberOne + numberTwo;
     }
 
-    //Method to Subtract two numbers
+    // Method to Subtract two numbers
     public static int Subtract(int numberOne, int numberTwo) {
       return numberOne - numberTwo;
     }
@@ -17,13 +22,13 @@
 
     // Method to Divide two numbers
     public static int Divide(int numberOne, int numberTwo) {
-        // Check to make sure that user is not trying to divide by 0
-        if (0 != numberTwo){
-            return numberOne / numberTwo;
-        }else{
-            // return 0 if user is trying to divide by 0 to stop crashes
-            return 0;
-        }
+      // Check to make sure that user is not trying to divide by 0
+      if (numberTwo != 0) {
+        return numberOne / numberTwo;
+      }
+
+      // return 0 if user is trying to divide by 0 to stop crashes
+      return 0;
     }
   }
 }
--- TicTacToe.Tests/CalculatorTest.cs
+++ TicTacToe.Tests/CalculatorTest.cs
@@ -8,92 +8,93 @@ namespace TicTacToe.Tests {
     public void AddTwoNumbers() {
       // Add two positve numbers
       Assert.AreEqual(Calculator.Add(10, 1), 11);
-            
+
       // Add two negative numbers
       Assert.AreEqual(Calculator.Add(-10, -1), -11);
-            
+
       // Add a negative number and positive number
       Assert.AreEqual(Calculator.Add(-10, 1), -9);
-            
+
       // Add two large numbers
       Assert.AreEqual(Calculator.Add(1000000, 100000), 1100000);
-            
+
       // Add two large negative numbers
       Assert.AreEqual(Calculator.Add(-1000000, -100000), -1100000);
-    
+
       // Add a large negative number and large positive number
       Assert.AreEqual(Calculator.Add(-1000000, 100000), -900000);
     }
+
     [TestMethod]
     public void MultiplyTwoNumbers() {
-    
-        //Multiply two positive numbers
-        Assert.AreEqual(Calculator.Multiply(5, 10), 50);
+      //Multiply two positive numbers
+      Assert.AreEqual(Calculator.Multiply(5, 10), 50);
 
-        //Multiply two negative numbers numbers
-        Assert.AreEqual(Calculator.Multiply(-5, -10), 50);
+      //Multiply two negative numbers numbers
+      Assert.AreEqual(Calculator.Multiply(-5, -10), 50);
 
-        //Multiply one negative number and one positive number
-        Assert.AreEqual(Calculator.Multiply(-5, 10), -50);
+      //Multiply one negative number and one positive number
+      Assert.AreEqual(Calculator.Multiply(-5, 10), -50);
 
-        //Multiply two large positive numbers
-        Assert.AreEqual(Calculator.Multiply(50000, 20000), 1000000000);
+      //Multiply two large positive numbers
+      Assert.AreEqual(Calculator.Multiply(50000, 20000), 1000000000);
 
-        //Multiply two large negative numbers
-        Assert.AreEqual(Calculator.Multiply(-50000, -20000), 1000000000);
+      //Multiply two large negative numbers
+      Assert.AreEqual(Calculator.Multiply(-50000, -20000), 1000000000);
 
-        //Multiply one large negative number and one large postive number
-        Assert.AreEqual(Calculator.Multiply(-50000, 20000), -1000000000);
+      //Multiply one large negative number and one large postive number
+      Assert.AreEqual(Calculator.Multiply(-50000, 20000), -1000000000);
 
-        //Multiply two numbers one of them being zero
-        Assert.AreEqual(Calculator.Multiply(5, 0), 0);
+      //Multiply two numbers one of them being zero
+      Assert.AreEqual(Calculator.Multiply(5, 0), 0);
     }
+
     [TestMethod]
-    public void SubtractTwoNumbers(){
-        //Subtract two positive numbers
-        Assert.AreEqual(Calculator.Subtract(10, 5), 5);
+    public void SubtractTwoNumbers() {
+      //Subtract two positive numbers
+      Assert.AreEqual(Calculator.Subtract(10, 5), 5);
 
-        //Subtract two negative numbers
-        Assert.AreEqual(Calculator.Subtract(-10, -5), -5);
+      //Subtract two negative numbers
+      Assert.AreEqual(Calculator.Subtract(-10, -5), -5);
 
-        //Subtract one negative number and one positive number
-        Assert.AreEqual(Calculator.Subtract(10, -5), 15);
+      //Subtract one negative number and one positive number
+      Assert.AreEqual(Calculator.Subtract(10, -5), 15);
 
-        //Subtract two large positive numbers
-        Assert.AreEqual(Calculator.Subtract(1000000, 500000), 500000);
+      //Subtract two large positive numbers
+      Assert.AreEqual(Calculator.Subtract(1000000, 500000), 500000);
 
-        //Subtract two large negative numbers
-        Assert.AreEqual(Calculator.Subtract(-1000000, -500000), -500000);
+      //Subtract two large negative numbers
+      Assert.AreEqual(Calculator.Subtract(-1000000, -500000), -500000);
 
-        //Subtract one large negative number and one large positive number
-        Assert.AreEqual(Calculator.Subtract(1000000, -500000), 1500000);
+      //Subtract one large negative number and one large positive number
+      Assert.AreEqual(Calculator.Subtract(1000000, -500000), 1500000);
     }
+
     [TestMethod]
-    public void DivideTwoNumbers()
-    {
-        //Divide two positive numbers
-        Assert.AreEqual(Calculator.Divide(10, 5), 2);
+    public void DivideTwoNumbers() {
+      //Divide two positive numbers
+      Assert.AreEqual(Calculator.Divide(10, 5), 2);
 
-        //Divide two negative numbers
-        Assert.AreEqual(Calculator.Divide(-10, -5), 2);
+      //Divide two negative numbers
+      Assert.AreEqual(Calculator.Divide(-10, -5), 2);
 
-        //Divide one negative number and one positive number
-        Assert.AreEqual(Calculator.Divide(10, -5), -2);
+      //Divide one negative number and one positive number
+      Assert.AreEqual(Calculator.Divide(10, -5), -2);
 
-        //Divide two large positive numbers
-        Assert.AreEqual(Calculator.Divide(10000000, 5000000), 2);
+      //Divide two large positive numbers
+      Assert.AreEqual(Calculator.Divide(10000000, 5000000), 2);
 
-        //Divide two large negative numbers
-        Assert.AreEqual(Calculator.Divide(-10000000, -5000000), 2);
+      //Divide two large negative numbers
+      Assert.AreEqual(Calculator.Divide(-10000000, -5000000), 2);
 
-        //Divide one large negative number and one large positive number
-        Assert.AreEqual(Calculator.Divide(10000000, -5000000), -2);
+      //Divide one large negative number and one large positive number
+      Assert.AreEqual(Calculator.Divide(10000000, -5000000), -2);
 
-        //Divide two numbers the second number being zero
-        Assert.AreEqual(Calculator.Divide(10, 0), 0);
+      //Divide two numbers the second number being zero
+      Assert.AreEqual(Calculator.Divide(10, 0), 0);
 
-        //Divide two numbers the first number being zero
-        Assert.AreEqual(Calculator.Divide(0, 10), 0);
+      //Divide two numbers the first number being zero
+      Assert.AreEqual(Calculator.Divide(0, 10), 0);
     }
   }
 }
