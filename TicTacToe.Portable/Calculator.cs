f9983e797f24d4f5ab2d546771c72307a83fb539
Viv-Rajkumar <viv.rajkumar@maidsafe.net>
2014-10-01 10:22:16 +0100

    Added sample calculator class for unit tests.

---
+++ TicTacToe.Portable/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace TicTacToe.Portable {
+  public class Calculator {
+    public static int Add(int numberOne, int numberTwo) {
+      return numberOne + numberTwo;
+    }
+
+    public static int Subtract(int numberOne, int numberTwo) {
+      return numberOne - numberTwo;
+    }
+
+    public static int Multiply(int numberOne, int numberTwo) {
+      return numberOne * numberTwo;
+    }
+
+    public static int Divide(int numberOne, int numberTwo) {
+      return numberOne / numberTwo;
+    }
+  }
+}
---
+++ TicTacToe.Tests/CalculatorTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe.Portable;
+
+namespace TicTacToe.Tests {
+  [TestClass]
+  public class CalculatorTest {
+    [TestMethod]
+    public void AddTwoNumbers() {
+      // add two positve numbers
+      Assert.AreEqual(Calculator.Add(10, 1), 11);
+
+      // add two negative numbers
+      Assert.AreEqual(Calculator.Add(-10, -1), -11);
+
+      // add a negative and positive number
+      Assert.AreEqual(Calculator.Add(-10, 1), -9);
+
+      // add two large numbers
+      Assert.AreEqual(Calculator.Add(1000000, 100000), 1100000);
+
+      // add two large negative numbers
+      Assert.AreEqual(Calculator.Add(-1000000, -100000), -1100000);
+
+      // add a large negative and large positive number
+      Assert.AreEqual(Calculator.Add(-1000000, 100000), -900000);
+    }
+
+    [TestMethod]
+    public void MultiplyTwoNumbers() {}
+  }
+}
