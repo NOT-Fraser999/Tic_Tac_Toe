7dbd9a8c556c2999b35bb23c0092d39c240f6599
NOT-Fraser999 <frasle.dasle@gmail.com>
2014-10-04 16:14:25 +0100

    MY Tests

---
+++ TicTacToe.Tests/TicTacToeFraserTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe.Portable;
+
+namespace TicTacToe.Tests
+{
+  [TestClass]
+  public class TicTacToeFraserTest
+  {
+    [TestMethod]
+    public void CheckforNewMove()
+    {
+
+
+
+    }
+
+    [TestMethod]
+    public void CheckforWinner() 
+    {
+
+
+
+    }
+
+    [TestMethod]
+    public void TogglePlayers()
+    {
+
+
+
+    }
+
+    [TestMethod]
+    public void ChangeWhoStartsNextRound()
+    {
+
+
+
+    }
+
+    [TestMethod]
+    public void CheckforCellReplacement()
+    {
+
+
+
+    }
+
+    [TestMethod]
+    public void IncrementtheWinCounter()
+    {
+      int startingWinCountX = TicTacToeGame.WinCountX;
+      int startingWinCountO = TicTacToeGame.WinCountO;
+      int thisTestWinCountX = 0;
+      int thisTestWinCountO = 0;
+
+      for (int i = 0; i < 3; ++i)
+      {
+        var game = new TicTacToeGame();
+        Assert.AreEqual(TicTacToeGame.WinCountX, startingWinCountX + thisTestWinCountX);
+        Assert.AreEqual(TicTacToeGame.WinCountO, startingWinCountO + thisTestWinCountO);
+        var startingPlayer = game.PlayerTurn;
+        game.NewMoveMade(0, 0);
+        game.NewMoveMade(0, 1);
+        game.NewMoveMade(1, 0);
+        game.NewMoveMade(1, 1);
+
+        Assert.AreEqual(TicTacToeGame.WinCountX, startingWinCountX + thisTestWinCountX);
+        Assert.AreEqual(TicTacToeGame.WinCountO, startingWinCountO + thisTestWinCountO);
+        game.NewMoveMade(2, 0);
+        if (startingPlayer == Player.X)
+        {
+          ++thisTestWinCountX;
+        }
+        else
+        {
+          ++thisTestWinCountO;
+        }
+        Assert.AreEqual(TicTacToeGame.WinCountX, startingWinCountX + thisTestWinCountX);
+        Assert.AreEqual(TicTacToeGame.WinCountO, startingWinCountO + thisTestWinCountO);
+      }
+    }
+
+    [TestMethod]
+    public void ShowWinState()
+    {
+
+
+
+    }
+  }
+}
