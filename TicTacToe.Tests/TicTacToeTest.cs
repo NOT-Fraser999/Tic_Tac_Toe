0cef6293f569d22466b841c7038d344fb6387bdb
NOT-Fraser999 <frasle.dasle@gmail.com>
2014-10-04 15:48:03 +0100

    Test Fix

--- TicTacToe.Tests/TicTacToeTest.cs
+++ TicTacToe.Tests/TicTacToeTest.cs
@@ -14,17 +14,25 @@ namespace TicTacToe.Tests {
     [TestMethod]
     public void CheckWinCounterForWin() {
       var game = new TicTacToeGame();
+      Assert.AreEqual(TicTacToeGame.WinCountX, 0);
+      Assert.AreEqual(TicTacToeGame.WinCountO, 0);
+      var startingPlayer = game.PlayerTurn;
       game.NewMoveMade(0, 0);
       game.NewMoveMade(0, 1);
       game.NewMoveMade(1, 0);
       game.NewMoveMade(1, 1);
 
       Assert.AreEqual(TicTacToeGame.WinCountX, 0);
-      game.NewMoveMade(2, 0);
-
-      Assert.AreEqual(TicTacToeGame.WinCountX, 1);
-
       Assert.AreEqual(TicTacToeGame.WinCountO, 0);
+      game.NewMoveMade(2, 0);
+      if (startingPlayer == Player.X)
+      {
+        Assert.AreEqual(TicTacToeGame.WinCountX, 1);
+        Assert.AreEqual(TicTacToeGame.WinCountO, 0);
+      } else {
+        Assert.AreEqual(TicTacToeGame.WinCountX, 0);
+        Assert.AreEqual(TicTacToeGame.WinCountO, 1);
+      }
     }
   }
 }
