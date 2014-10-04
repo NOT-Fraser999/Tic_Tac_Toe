18f3a2e2cf6822c8f743e8e42dd12d81585b3b85
Night-Shade <xiwmxwilliamson@gmail.com>
2014-10-04 23:01:46 +0100

    Added TicTacToeTestRyan.cs with a few Unit Tests

---
+++ TicTacToe.Tests/TicTacToeTestRyan.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe.Portable;
+
+namespace TicTacToe.Tests
+{
+    [TestClass]
+    public class TicTacToeTestRyan
+    {
+        [TestMethod]
+        public void CheckingNewGamePlayer() {
+            var game = new TicTacToeGame();
+            
+            Assert.AreEqual(game.PlayerTurn, Player.X);
+
+            var game2 = new TicTacToeGame();
+
+            Assert.AreEqual(game2.PlayerTurn, Player.O);
+        }
+
+        [TestMethod]
+        public void CheckingAlternatingTurns() {
+            var game = new TicTacToeGame();
+
+            Assert.AreEqual(game.PlayerTurn, Player.X);
+            game.NewMoveMade(0, 0);
+
+            Assert.AreEqual(game.PlayerTurn, Player.O);
+            game.NewMoveMade(1, 0);
+
+            Assert.AreEqual(game.PlayerTurn, Player.X);
+        }
+
+        [TestMethod]
+        public void CellOnlyClickableOnce() {
+            var game = new TicTacToeGame();
+
+            game.NewMoveMade(0, 0);
+            Assert.AreEqual(game.PlayerTurn, Player.X);
+
+            game.NewMoveMade(0, 0);
+            Assert.AreEqual(game.PlayerTurn, Player.X);
+        }
+    }
+}
